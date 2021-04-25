18ff1fdc1292aedfd4d7d71e210f74252d6a8fa9
KatyaChe <77936323+KatyaChe@users.noreply.github.com>
2021-04-25 20:25:43 +0300

    Add files via upload

---
+++ vvv.js
@@ -0,0 +1,21 @@
+button.onclick = function() {
+    let a = prompt ("Первое число?", );
+    let b = prompt ("Второе число?", );
+    alert (Number(a)+Number(b));
+}
+  button1.onclick = function() {
+    let a = prompt ("Первое число?", );
+    let b = prompt ("Второе число?", );
+    alert (Number(a)-Number(b));
+  };
+  button2.onclick = function() {
+    let a = prompt ("Первое число?", );
+    let b = prompt ("Второе число?", );
+    alert (Number(a)*Number(b));
+  };
+  button3.onclick = function() {
+    let a = prompt ("Первое число?", );
+    let b = prompt ("Второе число?", );
+    alert (Number(a)/Number(b));
+  };
+
